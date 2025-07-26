f1db9e830ddc840f0bdd285d5c2028ddd185724e
Kazuki <maheswara.abrisam2021@gmail.com>
2025-07-26 15:53:10 +0700

    Update style.css

--- style.css
+++ style.css
@@ -1,1 +1,16 @@
+body {
+  font-family: sans-serif;
+  text-align: center;
+  background: #f4f4f4;
+}
+textarea {
+  width: 80%;
+  height: 100px;
+  margin: 10px;
+  font-size: 16px;
+}
+button {
+  padding: 10px 20px;
+  font-size: 18px;
+}
 
